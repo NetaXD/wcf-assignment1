6f92d1a09265425560df6df5b7aa3683f32b6dfd
NateMrakava <nmrakava1342@conestogac.on.ca>
2022-10-16 19:08:44 -0400

    Add project files.

---
+++ A1Service/App_Code/Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+public class Service : IService {
+    public string GetData(int value) {
+        return string.Format("You entered: {0}", value);
+    }
+}
---
+++ Prog_3170_A1_Nate_Mrakava/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3170_A1_Nate_Mrakava {
+    internal class Program {
+        static void Main(string[] args) {
+            A1Service.ServiceClient srv = new A1Service.ServiceClient();
+            Console.WriteLine(srv.GetData(1));
+            Console.ReadKey();
+        }
+    }
+}
+
